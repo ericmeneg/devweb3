4317aa92cea4a11b09386730051c0edbfb04fbd8
ericmeneg <ericvinicius.menegon@gmail.com>
2025-03-21 07:56:49 -0300

    Adicionando projeto de nestjs

---
+++ workshop-nestjs/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { config } from './ormconfig';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forRoot(config), UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
